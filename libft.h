1a0fe33922c2ddefc531f39cb01b6fafedccce7d
esupatae <esupatae@student.42.us.org>
2019-11-08 20:13:08 -0800

    Adding ft_lstpop and get_next_line to my main library

--- libft.h
+++ libft.h
@@ -16,6 +16,17 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# define BUFF_SIZE	8
+
+typedef struct	s_store
+{
+	int		fd;
+	char	*rem;
+}				t_store;
 
 typedef struct	s_list
 {
@@ -103,6 +114,8 @@ void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+void			ft_lstpop(t_list **alst, t_list **elem,
+											void (*del)(void *, size_t));
 
 /*
 ** ------------------------------ EXTRA FUNCTIONS --------------------------
@@ -112,5 +125,6 @@ int				ft_pow(int x, int y);
 int				ft_abs(int n);
 int				ft_get_int_len(int n);
 int				ft_skip_char(char **ptr, char c);
+int				get_next_line(const int fd, char **line);
 
 #endif
